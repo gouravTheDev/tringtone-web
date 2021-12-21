1ad9f4259b7803b1e00850709fddb4c3fa1fd104
gouravTheDev <chatterjeegouravking@gmail.com>
2021-12-22 00:23:38 +0530

    Work goinf on

---
+++ index.php
@@ -0,0 +1,293 @@
+<?php
+include 'header.php';
+?>
+
+
+
+<div class="banner-carousel banner-carousel-2 mb-0">
+    <div class="banner-carousel-item" style="background-image: url(images/slider-main/bg1.jpg)">
+        <div class="container">
+            <div class="box-slider-content">
+                <div class="box-slider-text">
+                    <h2 class="box-slide-title">Tringtone</h2>
+                    <h3 class="box-slide-sub-title">End Terminal Specialist</h3>
+                    <p class="box-slide-description">
+                        Fashionable headset with superior sound and flexibility. Cancellation of surrounding noise improves talking efficiency.
+                    </p>
+                    <p>
+                        <a href="#" class="slider btn btn-primary">Clarity Affords Focus</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="banner-carousel-item" style="background-image: url(images/slider-main/bg3.jpg)">
+        <div class="slider-content text-left">
+            <div class="container">
+                <div class="box-slider-content">
+                    <div class="box-slider-text">
+                        <h2 class="box-slide-title">When Quality Matters</h2>
+                        <h3 class="box-slide-sub-title">
+                            Cost Effective Call Centers Headsets
+                        </h3>
+                        <p class="box-slide-description">
+                            A well working headset is a mandatory thing in any Call Center. Tringtone provides best Call Center Headsets at a reasonable cost.
+                        </p>
+                        <p>
+                            <a href="#" class="slider btn btn-primary" aria-label="about-us">See More</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="call-to-action-box no-padding">
+    <div class="container">
+        <div class="action-style-box">
+            <div class="row align-items-center">
+                <div class="col-md-8 text-center text-md-left">
+                    <div class="call-to-action-text">
+                        <h3 class="action-title">
+                            We understand your needs on corporate communication
+                        </h3>
+                    </div>
+                </div>
+                <!-- Col end -->
+                <div class="col-md-4 text-center text-md-right mt-3 mt-md-0">
+                    <div class="call-to-action-btn">
+                        <a class="btn btn-dark" href="#">Contact Us</a>
+                    </div>
+                </div>
+                <!-- col end -->
+            </div>
+            <!-- row end -->
+        </div>
+        <!-- Action style box -->
+    </div>
+    <!-- Container end -->
+</section>
+<!-- Action end -->
+
+<section class="our-quality">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <h2 class="section-title">We take pride at</h2>
+                <h3 class="section-sub-title">Our Qualities</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-12 mb-3">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="images/quality/price.png" style="height: 80px;" alt="">
+                            <h4 class="mt-3">Affordable Prices</h4>
+                            <p>Our headsets are quite affordable. Clarity does not depend on price. We never compromises with our headset quality.</p>
+                            <a href="#" class="btn btn-primary">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 mb-3">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="images/quality/technical-support.png" style="height: 80px;" alt="">
+                            <h4 class="mt-3">How can we help?</h4>
+                            <p>Tringtone customer support is available at your door step. Our customer is our first priority.</p>
+                            <a href="#" class="btn btn-primary">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 mb-3">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="images/quality/warranty.png" style="height: 80px;" alt="">
+                            <h4 class="mt-3">Extensive warranty</h4>
+                            <p>TringTone Limited Warranty shall apply to purchases products. You will be worry free in the warranty period.</p>
+                            <a href="#" class="btn btn-primary">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="ts-features" class="ts-features solid-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="ts-intro">
+                    <h2 class="into-title">About Us</h2>
+                    <h3 class="into-sub-title">We deliver quality products</h3>
+                    <p>
+                        We are rethoric question ran over her cheek When she reached
+                        the first hills of the Italic Mountains, she had a last view
+                        back on the skyline of her hometown Bookmarksgrove, the
+                        headline of Alphabet Village and the subline of her own road,
+                        the Line Lane.
+                    </p>
+                </div>
+                <!-- Intro box end -->
+
+                <div class="gap-20"></div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="ts-service-box">
+                            <span class="ts-service-icon">
+                                <i class="fas fa-trophy"></i>
+                            </span>
+                            <div class="ts-service-box-content">
+                                <h3 class="service-box-title">
+                                    We've Repution for Excellence
+                                </h3>
+                            </div>
+                        </div>
+                        <!-- Service 1 end -->
+                    </div>
+                    <!-- col end -->
+
+                    <div class="col-md-6">
+                        <div class="ts-service-box">
+                            <span class="ts-service-icon">
+                                <i class="fas fa-sliders-h"></i>
+                            </span>
+                            <div class="ts-service-box-content">
+                                <h3 class="service-box-title">We Build Partnerships</h3>
+                            </div>
+                        </div>
+                        <!-- Service 2 end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+                <!-- Content row 1 end -->
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="ts-service-box">
+                            <span class="ts-service-icon">
+                                <i class="fas fa-thumbs-up"></i>
+                            </span>
+                            <div class="ts-service-box-content">
+                                <h3 class="service-box-title">Guided by Commitment</h3>
+                            </div>
+                        </div>
+                        <!-- Service 1 end -->
+                    </div>
+                    <!-- col end -->
+
+                    <div class="col-md-6">
+                        <div class="ts-service-box">
+                            <span class="ts-service-icon">
+                                <i class="fas fa-users"></i>
+                            </span>
+                            <div class="ts-service-box-content">
+                                <h3 class="service-box-title">A Team of Professionals</h3>
+                            </div>
+                        </div>
+                        <!-- Service 2 end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+                <!-- Content row 1 end -->
+            </div>
+            <!-- Col end -->
+        </div>
+        <!-- Row end -->
+    </div>
+    <!-- Container end -->
+</section>
+<!-- Feature are end -->
+
+<section id="project-area" class="project-area solid-bg">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <h2 class="section-title">Work of Excellence</h2>
+                <h3 class="section-sub-title">Recent Products</h3>
+            </div>
+        </div>
+        <!--/ Title row end -->
+
+        <div class="row">
+            <div class="col-md-6 col-sm-12 mt-3">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <img src="images/products/product1.jpg" style="width: 100%;">
+                        <h2 class="mt-2 text-center">Tringtone Sync 20</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                        <a href="/product-single.html" class="btn btn-primary">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12 mt-3">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <img src="images/products/product1.jpg" style="width: 100%;">
+                        <h2 class="mt-2 text-center">Tringtone Studio P5</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                        <a href="/product-single.html" class="btn btn-primary">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="general-btn text-center">
+                    <a class="btn btn-primary" href="#">View All Products</a>
+                </div>
+            </div>
+        </div>
+        <!-- Content row end -->
+    </div>
+    <!--/ Container end -->
+</section>
+<!-- Project area end -->
+
+<section class="subscribe no-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="subscribe-call-to-acton">
+                    <h3>Need Guidance?</h3>
+                    <h4>(+9) 989-789-4598</h4>
+                </div>
+            </div>
+            <!-- Col end -->
+
+            <div class="col-lg-8">
+                <div class="ts-newsletter row align-items-center">
+                    <div class="col-md-5 newsletter-introtext">
+                        <h4 class="text-white mb-0">Newsletter Sign-up</h4>
+                        <p class="text-white">Latest updates and news</p>
+                    </div>
+
+                    <div class="col-md-7 newsletter-form">
+                        <form action="#" method="post">
+                            <div class="form-group">
+                                <label for="newsletter-email" class="content-hidden">Newsletter Email</label>
+                                <input type="email" name="email" id="newsletter-email" class="form-control form-control-lg" placeholder="Your your email and hit enter" autocomplete="off" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- Newsletter end -->
+            </div>
+            <!-- Col end -->
+        </div>
+        <!-- Content row end -->
+    </div>
+    <!--/ Container end -->
+</section>
+<!--/ subscribe end -->
+
+
+
+<?php include 'footer.php'; ?>
